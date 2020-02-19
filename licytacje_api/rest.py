b2bd067aec85c6b7b830b20844cf39fa57d7f392
Grzegorz Mianowski <g.mianowski@xberry.tech>
2020-02-19 01:42:43 +0100

    __RequestVerificationToken defeated after https introduction

--- licytacje_api/rest.py
+++ licytacje_api/rest.py
@@ -2,6 +2,7 @@ import os
 import requests
 import unicodedata
 import urllib.parse
+from pathlib import Path
 
 from babel.numbers import parse_decimal
 from datetime import date
@@ -9,12 +10,17 @@ from datetime import datetime
 
 import notice
 from bs_helpers import make_soup
+from bs4 import BeautifulSoup
 
+NODE_URL = "https://licytacje.komornik.pl"
 
-NODE_URL = "http://www.licytacje.komornik.pl"
-
-def get_search_soup(data):
-    return make_soup(requests.post(NODE_URL+"/Notice/Search", data=data))
+def get_search_soup(data: dict):
+    initial_resp = requests.get(NODE_URL+"/Notice/Search")
+    initial_soup = make_soup(initial_resp)
+    ver_token = get_verification_token(initial_soup)
+    data["__RequestVerificationToken"] = ver_token
+    resp = requests.post(NODE_URL+"/Notice/Search", data=data, cookies=initial_resp.cookies)
+    return make_soup(resp)
 
 def get_notice_soup(id):
     return make_soup(requests.get(NODE_URL+"/Notice/Details/"+str(id)))
@@ -29,6 +35,10 @@ def get_rows(html_soup):
     
 def get_cols(row):
     return row.find_all("td")
+
+def get_verification_token(html_soup: BeautifulSoup) -> str: 
+    ver_token = html_soup.find("input", attrs={"name":"__RequestVerificationToken"})
+    return ver_token.attrs["value"]
 
 def get_notices(html_soup):
     notices = html_soup.find_all("a", href=lambda href: href and "/Notice/Details" in href)
@@ -48,9 +58,9 @@ def get_report(data: dict) -> dict:
     auctions = []
     for row in rows[1:]:
         cols = get_cols(row)
-        if (len(cols) > 7):
+        if (len(cols) > 8):
             
-            url = NODE_URL + cols[7].contents[1].get('href')
+            url = NODE_URL + cols[8].contents[1].get('href')
             cur_notice = notice.Notice(url)
             auction = {
                 'date': datetime.strptime(cols[2].get_text(strip=True),"%d.%m.%Y"),
@@ -62,12 +72,17 @@ def get_report(data: dict) -> dict:
             auction['id'] = auction['date'].strftime("%Y%m%d")+"-"+str(auction["price"].to_integral_exact())
             auctions.append(auction)
 
-            file_path = os.path.join("..","notices", auction['id'])
+            files_dir = os.path.join("..","notices")
+            Path(files_dir).mkdir(parents=True, exist_ok=True)
+            file_path = os.path.join(files_dir, auction['id'])
             cur_notice.save_to_file(file_path)
     return auctions
 
 
 if __name__=="__main__":
+    # data = {'City': ['Wrocław'], 'Type': ['1']}
+    # search_soup = get_search_soup(data)
+
     import sqlite3
     import json
     from database import open_db, populate_db, create_db_table
